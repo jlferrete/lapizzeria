65690fa7b60f3cef233ff4fbfee6c97b178afd2c
jlferrete <joseluisferrete@gmail.com>
2020-03-22 19:05:13 +0100

    Created Header

---
+++ wp-content/themes/lapizzeria/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+<header class="site-header">
+    <div class="logo">
+        <a href="<?php echo esc_url( home_url( '/' ) ) ?>">
+            <img src="<?php echo get_template_directory_uri() ?>/img/logo.svg" alt="logo">
+        </a>
+    </div>
+
+    <div class="informacion-header">
+        <div class="redes-sociales">
+            <!-- TODO: Agregar menu -->
+        </div> <!--.redes-sociales  -->
+        
+        <div class="direccion">
+            <p>8179 Bay Avenue Mountain View, CA 94043</p>
+            <p>Teléfono: 555-666777</p>
+        </div>
+
+
+
+
+     </div> <!--.informacion-header -->
+
+</header>
+    
--- wp-content/themes/lapizzeria/page.php
+++ wp-content/themes/lapizzeria/page.php
@@ -1,3 +1,5 @@
+<?php get_header(); ?>
+
 <?php while(have_posts()): the_post(); ?>
 
     <h1><?php the_title(); ?></h1> 
