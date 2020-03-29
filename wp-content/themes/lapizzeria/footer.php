b535918393676cdacea08e441e9e1b8f70ae342b
jlferrete <joseluisferrete@gmail.com>
2020-03-29 13:12:57 +0200

    Changed footer

--- wp-content/themes/lapizzeria/footer.php
+++ wp-content/themes/lapizzeria/footer.php
@@ -1,3 +1,20 @@
+    <footer class="site-footer">
+        <?php
+            $args = array(
+                'theme_location' => 'header-menu',
+                'container' => 'nav',
+                'container_class' => 'footer-nav',
+                'after' => '<span class="separador"> | </span>'
+            );
+            wp_nav_menu( $args );
+        ?>
+
+        <div class="direccion">
+            <p>8179 Bay Avenue Mountain View, CA 94043</p>
+            <p>Teléfono: 555-666777</p>
+        </div>
+    </footer>
+    
     <?php wp_footer(); ?>
 </body>
 </html>
