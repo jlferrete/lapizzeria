7b3704a86a91643c2c66c616e144d97006495119
jlferrete <joseluisferrete@gmail.com>
2020-03-31 22:29:36 +0200

    Blog widgets done

--- wp-content/themes/lapizzeria/functions.php
+++ wp-content/themes/lapizzeria/functions.php
@@ -46,6 +46,21 @@ function lapizzeria_menus() {
 }
 add_action('init', 'lapizzeria_menus');
 
+/** Zona de widgets */
+
+function lapizzeria_widgets() {
+    register_sidebar(array (
+        'name'          =>  'Blog Sidebar',
+        'id'            =>  'blog_sidebar',
+        'before_widget' =>  '<div class="widget">',
+        'after_widget'  =>  '</div>',
+        'before_title'  =>  '<h3>',
+        'after_title'   =>  '</h3>'
+
+    ) );
+}
+add_action('widgets_init', 'lapizzeria_widgets');
+
 /** Agregar botones a paginador */
 
 function lapizzeria_botones_paginador() {
