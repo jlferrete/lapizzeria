25d3fd7531f591b0c10fbd3db8830e2a7ff8e1f9
jlferrete <joseluisferrete@gmail.com>
2020-04-01 20:23:24 +0200

    WIP Blog entries template

--- wp-content/themes/lapizzeria/home.php
+++ wp-content/themes/lapizzeria/home.php
@@ -20,24 +20,7 @@
                     <?php the_post_thumbnail('especialidades'); ?>
                 </a>
 
-                <header class="informacion-entrada">
-                    <div class="fecha">
-                        <time>
-                            <?php echo the_time('d'); ?>
-                            <span> <?php echo the_time('M'); ?> </span>
-                        </time>
-                    </div>
-                    <div class="titulo-entrada">
-                        <h2><?php the_title(); ?></h2>
-                    </div>
-                </header>
-
-                <p class="autor">
-                    Escrito por:
-                    <span>
-                        <?php the_author(); ?>
-                    </span>
-                </p>
+                <?php get_template_part('template-parts/informacion', 'entrada'); ?>
 
                 <div class="contenido-entrada">
                     <?php the_excerpt(); ?>
