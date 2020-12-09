dfd27023088988307079990f70bf0838f8a7dda6
jlferrete <joseluisferrete@gmail.com>
2020-12-09 19:11:04 +0100

    WIP: Plugin for Gutenberg blocks

---
+++ wp-content/plugins/lapizzeria-gutenberg/lapizzeria-gutenberg.php
@@ -0,0 +1,13 @@
+<?php
+/*
+    Plugin Name: La Pizzeria Gutenberg Block
+    Plugin URI:
+    Description: Agrega bloques de Gutenberg nativos
+    Version: 1.0
+    Author: Jose Luis Ferrete
+    Author URI: https://jlferrete.com/
+    License: GPL2
+    License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+if (!defined('ABSPATH')) exit;
